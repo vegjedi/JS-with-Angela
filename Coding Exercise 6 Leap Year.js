19ac940fa7c0702c5f42c978d384a38d20046a82
Plant Power <cainveg@gmail.com>
2022-07-28 16:26:02 -0700

    Leap year check

---
+++ Coding Exercise 6 Leap Year.js
@@ -0,0 +1,12 @@
+function isLeap(year) {
+    var message = "Year " + year + " is a leap year!"
+    if ((year % 4) == 0 && (year % 100 != 0)) {
+        alert(message)
+    } if ((year % 100 == 0) && (year % 400 == 0)) {
+        alert(message)
+    } else {
+        alert("Year " + year + " is NOT a leap year!")
+    }
+}
+
+isLeap(prompt("Please enter the year to check"))
