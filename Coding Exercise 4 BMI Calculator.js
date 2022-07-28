bb942191e99cc5dbbe7da558c9e5c5b441dda185
Plant Power <cainveg@gmail.com>
2022-07-28 16:11:36 -0700

    update BMI with if else

--- Coding Exercise 4 BMI Calculator.js
+++ Coding Exercise 4 BMI Calculator.js
@@ -1,8 +1,13 @@
 function bmiCalculator(weight, height) {
-    var bmi = weight/(height*height);
-    return bmi.toFixed(1)
+    var bmi = (weight/(height*height)).toFixed(1);
+    if (bmi < 18.5) {
+        alert("Your BMI is " + bmi + " ,so you are underweight")
+    } else if (bmi <= 24.9) {
+        alert("Your BMI is " + bmi + " ,so you have a normal weight")
+    } else {
+        alert("Your BMI is " + bmi + " ,so you are overweight")
+    }
 }
 
-alert("This is your BMI index " + 
-    bmiCalculator(prompt("What is your weight in kg?"), 
+(bmiCalculator(prompt("What is your weight in kg?"), 
     prompt("What is your height in meter")))
