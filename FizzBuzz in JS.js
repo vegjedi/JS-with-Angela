02619fb7a499840372c73e6178987dacf5413f5e
Plant Power <cainveg@gmail.com>
2022-07-28 17:45:37 -0700

    While loop

--- FizzBuzz in JS.js
+++ FizzBuzz in JS.js
@@ -2,16 +2,21 @@ var myList = [];
 count = 0;
 
 function FizzBuzz() {
-    count += 1;
-    if (count % 3 === 0) {
-        if (count % 5 === 0) {
-            myList.push("FizzBuzz")
+    while (count < 100) {
+        count += 1;
+        if (count % 3 === 0) {
+            if (count % 5 === 0) {
+                myList.push("FizzBuzz")
+            } else {
+                myList.push("Fizz")
+            }
+        } else if (count % 5 === 0) {
+            myList.push("Buzz")
         } else {
-            myList.push("Fizz")
+            myList.push(count)
         }
-    } else if (count % 5 === 0) {
-        myList.push("Buzz")
-    } else {
-        myList.push(count)
     }
-}
+};
+
+FizzBuzz();
+alert(myList);
