e274369b7ac97a1fb3e3cebbfa405458c6f7494f
Plant Power <cainveg@gmail.com>
2022-08-02 14:27:39 -0700

    jQuery ex

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,13 @@
-$("h1").addClass("big-title margin-50");
+// $("h1").addClass("big-title margin-50");
+
+// $("h1").html("Good bye");
+
+$("button").text("$$$$$");
+
+$("h1").click(function(){
+    $(this).css("color","blue");
+});
+
+$("input").keydown(function(event){
+    $("h1").text(event.key);
+})
