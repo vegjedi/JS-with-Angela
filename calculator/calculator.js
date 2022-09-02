f0a8f80291edc0b40da18e3189250536acaab31a
Plant Power <cainveg@gmail.com>
2022-09-01 19:26:39 -0700

    done!

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -3,12 +3,16 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req, res){
-    res.send(num1)
+    console.log(req.body);
+    var calResult = Number(req.body.num1) + Number(req.body.num2);
+    res.send("Total result is " + calResult);
 });
 
 app.listen(port, () => {
