203af477b1fb94829314c5f6f5bc46292ba0a2b3
Plant Power <cainveg@gmail.com>
2022-09-01 20:51:52 -0700

    fine tune and make both apps work

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -5,26 +5,25 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 // Normal Calculator App
 
-// app.get("/", function(req, res){
-//   res.sendFile(__dirname + "/index.html");
-// });
+app.get("/calculator", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
 
-// app.post("/", function(req, res){
-//     console.log(req.body);
-//     var calResult = Number(req.body.num1) + Number(req.body.num2);
-//     res.send("Total result is " + calResult);
-// });
+app.post("/calculator", function(req, res){
+    console.log(req.body);
+    var calResult = Number(req.body.num1) + Number(req.body.num2);
+    res.send("Total result is " + calResult);
+});
 
 // BMI Calculator App
 
-app.get("/", function(req, res){
+app.get("/bmicalculator", function(req, res){
   res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
-app.post("/", function(req, res){
+app.post("/bmicalculator", function(req, res){
   console.log(req.body);
   var bmiResult = Number(req.body.weight)/Math.pow(Number(req.body.height),2);
   res.send("Your BMI is " + bmiResult.toFixed(1));
