22ef728a9e1a24f4a4d910fccafd858a3b1a87e6
Plant Power <cainveg@gmail.com>
2022-09-01 20:46:02 -0700

    finished BMI calculator server side

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -5,14 +5,29 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+// Normal Calculator App
+
+// app.get("/", function(req, res){
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+// app.post("/", function(req, res){
+//     console.log(req.body);
+//     var calResult = Number(req.body.num1) + Number(req.body.num2);
+//     res.send("Total result is " + calResult);
+// });
+
+// BMI Calculator App
+
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
 app.post("/", function(req, res){
-    console.log(req.body);
-    var calResult = Number(req.body.num1) + Number(req.body.num2);
-    res.send("Total result is " + calResult);
+  console.log(req.body);
+  var bmiResult = Number(req.body.weight)/Math.pow(Number(req.body.height),2);
+  res.send("Your BMI is " + bmiResult.toFixed(1));
 });
 
 app.listen(port, () => {
