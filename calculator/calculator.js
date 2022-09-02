2b194fc49fc6353d76d5999dab3ba2f4699d99cd
Plant Power <cainveg@gmail.com>
2022-09-01 18:47:11 -0700

    body-parser

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.get('/', function(req, res){
+app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+    res.send(num1)
 });
 
 app.listen(port, () => {
