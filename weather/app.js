f11bb20dad5d94315908e9b161c4976bb7aaeddc
Plant Power <cainveg@gmail.com>
2022-09-06 16:55:10 -0700

    parsing JSON API

--- weather/app.js
+++ weather/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const https = require("https");
+const https = require("node:https");
   
 const app = express();
 const port = 3000;
@@ -9,6 +9,13 @@ app.get("/", function(req, res){
     const url = "https://api.openweathermap.org/data/2.5/weather?q=vancouver&appid=6b6a817d74f2f9a04b1c87f61f227ed9&units=metric";
     https.get(url, function(response) {
       console.log(response.statusCode);
+      response.on('data', function(data) {
+        const weatherData = JSON.parse(data);
+        console.log(weatherData);
+        const temp = weatherData.main.temp;
+        const des = weatherData.weather[0].description
+        console.log(temp, des);
+      });
     });
   });
 
