1d6402570793293bc60d74e3298c3f28df6a61e4
Plant Power <cainveg@gmail.com>
2022-09-06 16:16:43 -0700

    commit

--- weather/app.js
+++ weather/app.js
@@ -6,14 +6,12 @@ const port = 3000;
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=vancouver&appid=6b6a817d74f2f9a04b1c87f61f227ed9&units=metric";
+    https.get(url, function(response) {
+      console.log(response.statusCode);
+    });
   });
 
-const url = "https://api.openweathermap.org/data/2.5/weather?q=vancouver&appid=6b6a817d74f2f9a04b1c87f61f227ed9&units=metric";
-
-https.get(url, function(response) {
-  console.log(response);
-});
-
 app.listen(port, function() {
-console.log("Server is running on port 3000")
-});
+  console.log("Server is running on port 3000")
+  });
