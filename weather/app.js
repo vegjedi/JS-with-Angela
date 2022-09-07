60539f6107167eeb59286c3a4dbe745a8e70c2bd
Plant Power <cainveg@gmail.com>
2022-09-07 16:50:03 -0700

    icon added

--- weather/app.js
+++ weather/app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
+    // res.sendFile(__dirname + "/index.html");
     const url = "https://api.openweathermap.org/data/2.5/weather?q=vancouver&appid=6b6a817d74f2f9a04b1c87f61f227ed9&units=metric";
     https.get(url, function(response) {
       console.log(response.statusCode);
@@ -13,8 +13,12 @@ app.get("/", function(req, res){
         const weatherData = JSON.parse(data);
         console.log(weatherData);
         const temp = weatherData.main.temp;
-        const des = weatherData.weather[0].description
-        console.log(temp, des);
+        const des = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon
+        res.write("<h1>Vancouver weather is " + temp + " degree Celcius.<h1>");
+        res.write("<p> And it is " + des + "<p>");
+        res.write("<img src=" + "http://openweathermap.org/img/wn/" + icon + "@2x.png>");
+        res.send()
       });
     });
   });
