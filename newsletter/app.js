c5cc155ed2653d2f8eb329d34f80acb0c6fc7e91
Plant Power <cainveg@gmail.com>
2022-09-07 20:20:11 -0700

    posting method with public folder

--- newsletter/app.js
+++ newsletter/app.js
@@ -4,11 +4,20 @@ const https = require("node:https");
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({extended: true }));
+
+app.use(express.static("public"));
 
 app.get('/', function(req, res){
-    res.send("Hello World");
+    res.sendFile(__dirname + "/signup.html");
 });
+
+app.post("/", function(req, responde){
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    console.log(firstName + lastName + email);
+      });
 
 app.listen(port, function(res){
     console.log("Listening to port 3000");
