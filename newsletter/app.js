2c8547a5a280bb6be69409f281fda84454591566
Plant Power <cainveg@gmail.com>
2022-09-07 20:44:08 -0700

    add mailchimp

--- newsletter/app.js
+++ newsletter/app.js
@@ -16,9 +16,28 @@ app.post("/", function(req, responde){
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const email = req.body.email;
-    console.log(firstName + lastName + email);
+    console.log(firstName, lastName, email);
       });
 
 app.listen(port, function(res){
     console.log("Listening to port 3000");
 });
+
+// df732574fbd71359ef23d37d8e90eafa-us9
+
+const client = require("mailchimp-marketing");
+
+client.setConfig({
+  apiKey: "YOUR_API_KEY",
+  server: "YOUR_SERVER_PREFIX",
+});
+
+const run = async () => {
+  const response = await client.lists.batchListMembers("list_id", {
+    members: [{}],
+  });
+  console.log(response);
+};
+
+run();
+
