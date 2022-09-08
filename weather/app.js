3da29b9333e7b3753fd2d2d7d69b768d88845ae8
Plant Power <cainveg@gmail.com>
2022-09-07 17:01:24 -0700

    index page for weather input

--- weather/app.js
+++ weather/app.js
@@ -5,8 +5,10 @@ const app = express();
 const port = 3000;
 
 app.get("/", function(req, res){
-    // res.sendFile(__dirname + "/index.html");
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=vancouver&appid=6b6a817d74f2f9a04b1c87f61f227ed9&units=metric";
+    const query = "Vancouver";
+    const apiKEY = "6b6a817d74f2f9a04b1c87f61f227ed9";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKEY + "&units=" + unit;
     https.get(url, function(response) {
       console.log(response.statusCode);
       response.on('data', function(data) {
@@ -15,7 +17,7 @@ app.get("/", function(req, res){
         const temp = weatherData.main.temp;
         const des = weatherData.weather[0].description;
         const icon = weatherData.weather[0].icon
-        res.write("<h1>Vancouver weather is " + temp + " degree Celcius.<h1>");
+        res.write("<h1> " + query + " weather is " + temp + " degree Celcius.<h1>");
         res.write("<p> And it is " + des + "<p>");
         res.write("<img src=" + "http://openweathermap.org/img/wn/" + icon + "@2x.png>");
         res.send()
