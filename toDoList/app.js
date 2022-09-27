2fb24be5d590f7c89eb6fb4e436001049fac9762
Plant Power <cainveg@gmail.com>
2022-09-26 17:11:39 -0700

    toDoList1

--- toDoList/app.js
+++ toDoList/app.js
@@ -4,7 +4,12 @@ const app = express();
 const port = 3000;
 
 app.get("/", function(req, res){
-    res.send("hello");
+    var today = new Date();
+    if (today.getDay() === 6 || today.getDay() === 0) {
+        res.send("Yay it's the weekend!");
+    } else {
+        res.send("I have to work today...");
+    }
   });
 
 app.listen(port, () => {
