e98a9dda6117ea6620337eeb2eb60569cde36c35
Plant Power <cainveg@gmail.com>
2022-09-27 16:46:24 -0700

    add draft list function

--- toDoList/app.js
+++ toDoList/app.js
@@ -3,7 +3,11 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
   var today = new Date();
@@ -17,8 +21,15 @@ app.get("/", function (req, res) {
   var day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
-    kindOfDay: day
+    kindOfDay: day,
+    newListItems: items
   });
+});
+
+app.post("/", function(req,res) {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
