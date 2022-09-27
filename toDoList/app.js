0e25352dc94947e079526d935f857ef3728669c8
Plant Power <cainveg@gmail.com>
2022-09-26 17:56:11 -0700

    add EJS

--- toDoList/app.js
+++ toDoList/app.js
@@ -3,15 +3,42 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.get("/", function(req, res){
-    var today = new Date();
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        res.send("Yay it's the weekend!");
-    } else {
-        res.send("I have to work today...");
-    }
+app.set('view engine', 'ejs');
+
+app.get("/", function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+  }
+
+  res.render("list", {
+    kindOfDay: day
   });
+});
 
 app.listen(port, () => {
-    console.log("App listening on port 3000")
-  });
+  console.log("App listening on port 3000")
+});
