62a4ffaa80ec3292a58db285ee3aad9c250363c2
Plant Power <cainveg@gmail.com>
2022-09-27 18:21:46 -0700

    complete toDoList

--- toDoList/app.js
+++ toDoList/app.js
@@ -3,22 +3,24 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function (req, res) {
-  var today = new Date();
-  var options = {
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  let today = new Date();
+  let options = {
     weekday: "long", 
     year: "numeric", 
     month: "long", 
     day: "numeric" 
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
     kindOfDay: day,
@@ -27,7 +29,7 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function(req,res) {
-  var item = req.body.newItem;
+  let item = req.body.newItem;
   items.push(item);
   res.redirect("/");
 });
