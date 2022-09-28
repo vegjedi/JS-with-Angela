05e3c9718d942b71af94a8654e548d8841e5fb6f
Plant Power <cainveg@gmail.com>
2022-09-27 20:03:09 -0700

    seperate footer header

--- toDoList/app.js
+++ toDoList/app.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems =[];
 
 app.set('view engine', 'ejs');
 
@@ -15,23 +16,36 @@ app.get("/", function(req, res) {
   let today = new Date();
   let options = {
     weekday: "long", 
-    year: "numeric", 
     month: "long", 
     day: "numeric" 
   };
 
   let day = today.toLocaleDateString("en-US", options);
-
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items
   });
 });
 
+app.get("/work", function(req,res){
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems})
+});
+
+app.get("/about", function(req,res){
+  res.render("about")
+});
+
 app.post("/", function(req,res) {
   let item = req.body.newItem;
+  if (req.body.button === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
   items.push(item);
   res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
